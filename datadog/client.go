26200b44d3fd09e5be6a45c1a0f76b6b8443346f
Tim Ehlers <ehlerst@yahoo.com>
2022-12-10 09:56:15 -0600

    MVP

---
+++ datadog/client.go
@@ -0,0 +1,266 @@
+// Copyright 2022 tehlers
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package datadog
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/prompb"
+	"golang.org/x/net/context"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV1"
+)
+
+// Client allows reading from datadog
+type Client struct {
+	logger 		log.Logger
+	context 	context.Context
+	client     *datadogV1.MetricsApi
+}
+
+// NewClient creates a new Client.
+func NewClient(logger log.Logger) *Client {
+	// The default automagicaly gets OS env var DD_API_KEY, DD_APP_KEY and DD_SITE
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV1.NewMetricsApi(apiClient)
+
+	if logger == nil {
+		logger = log.NewNopLogger()
+	}
+
+	return &Client{
+		logger:          logger,
+		context:         ctx,
+		client:          api,
+	}
+}
+
+// Read Gets a batch of samples to datadog.
+func (c *Client) Read(req *prompb.ReadRequest) (*prompb.ReadResponse, error) {
+	labelsToSeries := map[string]*prompb.TimeSeries{}
+	for _, q := range req.Queries {
+		level.Info(c.logger).Log("msg", "query`: %v\n", q)
+		nq, to, from, _ := c.buildQuery(q)
+		level.Info(c.logger).Log("msg", "new query`: %v\n", nq)
+		resp := c.runQuery(nq, to, from)
+
+		if err := mergeResult(labelsToSeries, []datadogV1.MetricsQueryResponse{resp}); err != nil {
+			return nil, err
+		}
+
+
+	}
+
+
+	resp := prompb.ReadResponse{
+		Results: []*prompb.QueryResult{
+			{Timeseries: make([]*prompb.TimeSeries, 0, len(labelsToSeries))},
+		},
+	}
+	for _, ts := range labelsToSeries {
+		resp.Results[0].Timeseries = append(resp.Results[0].Timeseries, ts)
+	}
+	return &resp, nil
+
+	
+//	resp, r, err := c.client.QueryMetrics(c.context, time.Now().AddDate(0, 0, -1).Unix(), time.Now().Unix(), "system.cpu.idle{*}")
+
+	return &prompb.ReadResponse{}, nil
+}
+
+func (c *Client)runQuery(query string, to int64, from int64)  datadogV1.MetricsQueryResponse {
+
+	resp, r, err := c.client.QueryMetrics(c.context, from, to, query)
+
+	if err != nil {
+		level.Debug(c.logger).Log("msg", "Error when calling `MetricsApi.QueryMetrics`: %v\n", err)
+		level.Debug(c.logger).Log("msg", "Full HTTP response: %v\n", r)
+	}
+	//labelsToSeries := map[string]*prompb.TimeSeries{}
+	//mergeResult(labelsToSeries, []datadogV1.MetricsQueryResponse{resp})
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	level.Info(c.logger).Log("msg","Response from `MetricsApi.QueryMetrics`:\n%s\n", responseContent)
+	return resp
+}
+
+// Name identifies the client as a Datadog client.
+func (c Client) Name() string {
+	return "datadog"
+}
+
+
+
+func mergeResult(labelsToSeries map[string]*prompb.TimeSeries, results []datadogV1.MetricsQueryResponse) error {
+	for _, r := range results {
+		for _, s := range r.Series {
+			ts, ok := labelsToSeries[*s.DisplayName]
+			if !ok {
+				ts = &prompb.TimeSeries{
+					Labels: nil,
+				}
+				labelsToSeries[*s.DisplayName] = ts
+			}
+
+			samples, err := valuesToSamples(s.Pointlist)
+			if err != nil {
+				return err
+			}
+
+			ts.Samples = mergeSamples(ts.Samples, samples)
+		}
+	}
+	return nil
+}
+
+
+func valuesToSamples(values [][]*float64) ([]prompb.Sample, error) {
+	samples := make([]prompb.Sample, 0, len(values))
+	for _, v := range values {
+		samples = append(samples, prompb.Sample{
+			Timestamp: int64(*v[0]),
+			Value:     *v[1],
+		})
+	}
+	return samples, nil
+}
+
+func tagsToLabelPairs(name string, tags map[string]string) []prompb.Label {
+	pairs := make([]prompb.Label, 0, len(tags))
+	for k, v := range tags {
+		if v == "" {
+			// If we select metrics with different sets of labels names,
+			// InfluxDB returns *all* possible tag names on all returned
+			// series, with empty tag values on series where they don't
+			// apply. In Prometheus, an empty label value is equivalent
+			// to a non-existent label, so we just skip empty ones here
+			// to make the result correct.
+			continue
+		}
+		pairs = append(pairs, prompb.Label{
+			Name:  k,
+			Value: v,
+		})
+	}
+	pairs = append(pairs, prompb.Label{
+		Name:  model.MetricNameLabel,
+		Value: name,
+	})
+	return pairs
+}
+
+func concatLabels(labels map[string]string) string {
+	// 0xff cannot occur in valid UTF-8 sequences, so use it
+	// as a separator here.
+	separator := "\xff"
+	pairs := make([]string, 0, len(labels))
+	for k, v := range labels {
+		pairs = append(pairs, k+separator+v)
+	}
+	return strings.Join(pairs, separator)
+}
+
+// mergeSamples merges two lists of sample pairs and removes duplicate
+// timestamps. It assumes that both lists are sorted by timestamp.
+func mergeSamples(a, b []prompb.Sample) []prompb.Sample {
+	result := make([]prompb.Sample, 0, len(a)+len(b))
+	i, j := 0, 0
+	for i < len(a) && j < len(b) {
+		if a[i].Timestamp < b[j].Timestamp {
+			result = append(result, a[i])
+			i++
+		} else if a[i].Timestamp > b[j].Timestamp {
+			result = append(result, b[j])
+			j++
+		} else {
+			result = append(result, a[i])
+			i++
+			j++
+		}
+	}
+	result = append(result, a[i:]...)
+	result = append(result, b[j:]...)
+	return result
+}
+
+
+func (c *Client) buildQuery(q *prompb.Query) (string, int64, int64, error) {
+	var ddogFormat string
+	matchers := make([]string, 0, len(q.Matchers))
+	//matchers := make([]string, 0, len(q.Matchers))
+	// convert to ddog format 
+	// example: system.cpu.idle{host:foo,cluster:bar}} == system_cpu_idle{host="foo",cluster="bar"}
+	for _, m := range q.Matchers {
+		if m.Name == model.MetricNameLabel {
+			switch m.Type {
+			case prompb.LabelMatcher_EQ:
+				ddogFormat = replaceDot(m.Value)
+			default:
+				return "", 0, 0, errors.New("name metric does not match anything")
+			}
+			continue
+		}
+
+		switch m.Type {
+		case prompb.LabelMatcher_EQ:
+			matchers = append(matchers, fmt.Sprintf("%s:'%s'", m.Name, m.Value))
+		case prompb.LabelMatcher_NEQ:
+			matchers = append(matchers, fmt.Sprintf("!%s:'%s'", m.Name, m.Value))
+		case prompb.LabelMatcher_RE:
+			matchers = append(matchers, fmt.Sprintf("%s:%s", m.Name, m.Value))
+		case prompb.LabelMatcher_NRE:
+			matchers = append(matchers, fmt.Sprintf("!%s:%s", m.Name, m.Value))
+		default:
+			return "", 0, 0, fmt.Errorf("unknown match type %v", m.Type)
+		}
+
+	}
+
+	tags := strings.Join(matchers, ",")
+	tags = strings.ReplaceAll(tags, "\"", "");
+	// Not sure why its adding the single quotes... anyways
+	tags = strings.ReplaceAll(tags, "'", "");
+	if tags == "" {
+		tags = "*"
+	}
+	ddogFormat = fmt.Sprintf(ddogFormat +  "{" + tags + "}")
+	// DDog wont take timestamps in ms....
+
+	// TODO probably check the values are valid. Maybe
+	to := toSeconds(q.EndTimestampMs)
+	from := toSeconds(q.StartTimestampMs)
+	return ddogFormat, to, from, nil
+
+
+//	return fmt.Sprintf("%s", strings.Join(matchers, " AND ")), nil
+}
+
+
+func toSeconds(millis int64) int64 {
+	return time.Unix(0, millis * int64(time.Millisecond)).Unix()
+}
+
+func replaceDot(str string) string {
+	return strings.Replace(str, `_`, `.`, -1)
+}
---
+++ go.mod
@@ -0,0 +1,65 @@
+module github.com/tehlers320/datadog-remote-adapter
+
+go 1.19
+
+require (
+	github.com/go-kit/kit v0.10.0
+	github.com/go-kit/log v0.2.1
+	github.com/gogo/protobuf v1.3.2
+	github.com/golang/snappy v0.0.4
+	github.com/pkg/errors v0.9.1
+	github.com/prometheus/client_golang v1.13.1
+	github.com/prometheus/common v0.38.0
+	github.com/prometheus/prometheus v0.40.6
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
+
+require (
+	github.com/DataDog/zstd v1.5.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+)
+
+require (
+	github.com/DataDog/datadog-api-client-go/v2 v2.5.0
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
+	github.com/aws/aws-sdk-go v1.44.128 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dennwc/varint v1.0.0 // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/grafana/regexp v0.0.0-20221005093135-b4c2bcb0a4b6 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.2 // indirect
+	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common/sigv4 v0.1.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.36.4 // indirect
+	go.opentelemetry.io/otel v1.11.1 // indirect
+	go.opentelemetry.io/otel/metric v0.33.0 // indirect
+	go.opentelemetry.io/otel/trace v1.11.1 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/goleak v1.2.0 // indirect
+	golang.org/x/exp v0.0.0-20221031165847-c99f073a8326 // indirect
+	golang.org/x/net v0.4.0
+	golang.org/x/oauth2 v0.1.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	golang.org/x/time v0.1.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/api v0.26.0 // indirect
+	k8s.io/apimachinery v0.26.0 // indirect
+	k8s.io/client-go v0.26.0 // indirect
+)
